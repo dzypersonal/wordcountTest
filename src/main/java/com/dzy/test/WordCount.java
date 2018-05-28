eeabbef6ec7df0ff71280bdd6450ae1dc52e0619
dengziyang <dengziyang@meicai.cn>
2018-05-28 17:29:54 +0800

    2

--- src/main/java/com/dzy/test/WordCount.java
+++ src/main/java/com/dzy/test/WordCount.java
@@ -1,15 +1,25 @@
 package com.dzy.test;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class WordCount {
+public class WordCount extends Configured implements Tool {
     public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new WordCount(), args);
+        System.exit(exitCode);
+
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
@@ -20,7 +30,6 @@ public class WordCount {
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        job.waitForCompletion(true) ;
-        //System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 }
